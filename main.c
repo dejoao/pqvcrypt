1205daf29085cb26b245a22ca97658e5e5c63bf8
Joao <joaopedro149893@gmail.com>
2025-01-13 02:28:33 -0300

    primeiro commit

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#define TAMANHO_ALFABETO 26
+
+int main(void)
+{
+    printf("Sua chave é \n");
+}
+
+char chave(void)
+{
+    //variavel com todas letras do alfabeto
+    const char alfabeto[] = 'abcdefghijklmnopqrstuvwxyz';
+    //cria uma nova variavel
+    char chave[];
+    for (int i = 0, i < TAMANHO_ALFABETO, i++)
+    {
+        //cria um indice aleatorio
+        int indice_aleatorio = 
+        //pega uma letra do alfabeto e adiciona na nova variavel atraves do indice aleatorio
+    }
+
+    //cria um caractere nulo no fim da string
+}
