fc2b83b6387db108c084d1f0d090b4fc05a03e4d
Joao <joaopedro149893@gmail.com>
2025-01-13 22:58:32 -0300

    add funcao chave

--- main.c
+++ main.c
@@ -1,24 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h> //usado na funcao chave
+#include <time.h>
 
 #define TAMANHO_ALFABETO 26
 
+char *chave(void);
+
 int main(void)
 {
-    printf("Sua chave é \n");
+    printf("Sua chave é %s\n", chave());
+    
 }
 
-char chave(void)
+char *chave(void)
 {
+    srand(time(NULL)); //usado para gerar um numero aleatorio com base no horario
     //variavel com todas letras do alfabeto
-    const char alfabeto[] = 'abcdefghijklmnopqrstuvwxyz';
+    const char alfabeto[] = "abcdefghijklmnopqrstuvwxyz";
     //cria uma nova variavel
-    char chave[];
-    for (int i = 0, i < TAMANHO_ALFABETO, i++)
+    char *chave = malloc(27 * sizeof(char));
+    for (int i = 0; i < TAMANHO_ALFABETO; i++)
     {
         //cria um indice aleatorio
-        int indice_aleatorio = 
+        int indice_aleatorio = rand() % TAMANHO_ALFABETO;
         //pega uma letra do alfabeto e adiciona na nova variavel atraves do indice aleatorio
+        chave[i] = alfabeto[indice_aleatorio];
     }
-
     //cria um caractere nulo no fim da string
+    chave[TAMANHO_ALFABETO + 1] = '\0';
+    return chave;
+    free(chave);
 }
